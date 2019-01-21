a402f6c95451bbb634346b7502360cfdd4755a29
Adrien Crivelli <adrien.crivelli@gmail.com>
2019-01-21 20:14:30 +1100

    Upgrade to latest JS deps
    This is to remove security issues in various deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,90 +1,93 @@
-var gulp = require('gulp');
+const {watch, parallel, series, src, dest} = require('gulp');
 
-var handleErrors = require('./gulp/handleErrors');
-var uglify = require('gulp-uglify');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var watch = require('gulp-watch');
-var browserSync = require('browser-sync');
-var rename = require("gulp-rename");
-var autoprefixer = require('autoprefixer');
-var postcss = require('gulp-postcss');
+const handleErrors = require('./gulp/handleErrors');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync');
+const rename = require('gulp-rename');
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
 
 /**
  * Config
  */
-var paths = {
+const paths = {
     scripts: 'src/**/*.js',
     styles: 'src/css/*.scss',
     themes: 'src/css/*/*.css',
     images: ['src/**/*.gif', 'src/**/*.jpg', 'src/**/*.jpeg', 'src/**/*.png'],
     dest: 'dist',
     sourcemaps: 'maps',
-    filename: 'natural-carousel'
+    filename: 'natural-carousel',
 };
 
 /**
  * Javascript
  */
-gulp.task('scripts', function() {
-    return gulp.src(paths.scripts)
-               .pipe(uglify().on('error', handleErrors))
-               .pipe(concat(paths.filename + ".min.js"))
-               .pipe(gulp.dest(paths.dest));
-});
+function scripts() {
+    return src(paths.scripts)
+        .pipe(uglify().on('error', handleErrors))
+        .pipe(concat(paths.filename + '.min.js'))
+        .pipe(dest(paths.dest));
+}
 
 /**
  * Stylesheets
  */
-gulp.task('styles', function() {
+function styles() {
 
-    gulp.src(paths.themes)
-        .pipe(gulp.dest(paths.dest));
+    src(paths.themes)
+        .pipe(dest(paths.dest));
 
-    return gulp.src(paths.styles)
-               .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
-               .pipe(postcss([autoprefixer()]).on('error', handleErrors))
-               .pipe(rename(paths.filename + ".min.css"))
-               .pipe(gulp.dest(paths.dest));
-});
+    return src(paths.styles)
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(postcss([autoprefixer()]).on('error', handleErrors))
+        .pipe(rename(paths.filename + '.min.css'))
+        .pipe(dest(paths.dest));
+}
 
 /**
  * Browsersync
  */
-gulp.task('bs', function() {
+function bs(cb) {
     browserSync({
-        proxy: 'natural-carousel.lan/demo',
-        host: 'natural-carousel.lan',
-        open: 'external'
+        server: true,
+        startPath: '/demo',
     });
-});
+    cb();
+}
 
 /**
  * Refresh
  */
-gulp.task('refresh', function() {
+function refresh(cb) {
     console.info('******************** REFRESH ***************************');
-});
+    cb();
+}
 
-gulp.task('reload', function() {
+function reload(cb) {
     browserSync.reload();
-});
+    cb();
+}
+
+function watchFiles() {
+    // Source files
+    watch(paths.styles, series(refresh, styles, reload));
+    watch(paths.themes, series(refresh, styles, reload));
+    watch(paths.scripts, series(refresh, scripts, reload));
+
+    // Demo files
+    watch('demo/*.html', series(refresh, reload));
+    watch('demo/*.css', series(refresh, reload));
+}
+
+const build = parallel(scripts, styles);
 
 /**
  * Main tasks
  */
-gulp.task('build', ['scripts', 'styles']);
-gulp.task('live', ['build', 'bs', 'watch']);
-gulp.task('watch', ['build'], function() {
-
-    // Source files
-    gulp.watch(paths.styles, ['refresh', 'styles', 'reload']);
-    gulp.watch(paths.themes, ['refresh', 'styles', 'reload']);
-    gulp.watch(paths.scripts, ['refresh', 'scripts', 'reload']);
-
-    // Demo files
-    gulp.watch('demo/*.html', ['refresh', 'reload']);
-    gulp.watch('demo/*.css', ['refresh', 'reload']);
-
-});
-
+exports.default = build;
+exports.build = build;
+exports.live = series(build, bs, watchFiles);
+exports.watch = series(build, watchFiles);
